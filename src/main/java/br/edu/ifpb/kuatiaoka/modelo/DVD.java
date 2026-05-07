37c5103112737dc272dfa1a8d94db60f6e12891a
PAULOLIMA6 <paulo.lima.6@academico.ifpb.edu.br>
2026-05-07 12:33:05 -0300

    re organizando classe DVD em secoes de atributos, construtor, getters e metodos

--- src/main/java/br/edu/ifpb/kuatiaoka/modelo/DVD.java
+++ src/main/java/br/edu/ifpb/kuatiaoka/modelo/DVD.java
@@ -1,20 +1,38 @@
 package br.edu.ifpb.kuatiaoka.modelo;
 
 public class DVD extends Item {
+    
+    // ========== ATRIBUTOS ==========
     private String diretor;
     private int duracao; // em minutos
     private String classificacao;
-
+    
+    // ========== CONSTRUTOR ==========
     public DVD(String titulo, String editora, String diretor, int duracao, String classificacao) {
         super(titulo, editora);
         this.diretor = diretor;
         this.duracao = duracao;
         this.classificacao = classificacao;
     }
-
-    // o prof pedia diretor e duracao pra dvd mas coloquei o super(titulo, editora) pq todo item precisa disso
-
-    public String getDiretor() { return diretor; }
-    public int getDuracao() { return duracao; }
-    public String getClassificacao() { return classificacao; }
+    
+    // ========== GETTERS ==========
+    
+    public String getDiretor() {
+        return diretor;
+    }
+    
+    public int getDuracao() {
+        return duracao;
+    }
+    
+    public String getClassificacao() {
+        return classificacao;
+    }
+    
+    // ========== MÉTODOS ==========
+    
+    @Override
+    public String toString() {
+        return "DVD: " + getTitulo() + " | Diretor: " + diretor + " | Duração: " + duracao + "min";
+    }
 }
